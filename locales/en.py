c7451b2d2c2b22012c88b420fa4382e2a4cc8b20
xsnuuks <xsnuuks@gmail.com>
2026-08-16 03:26:12 +0200

    Create en.py

---
+++ locales/en.py
@@ -0,0 +1,6 @@
+TEXTS = {
+    "choose_language": "Выберите язык / Choose language / Sprache wählen:",
+    "language_set": "Language set: English 🇬🇧",
+    "welcome": "Welcome to <b>MONO</b>.\n\nMinimalism. Quality. Only proven liquids.",
+    # ... остальные ключи можно скопировать из ru.py и перевести позже
+}
