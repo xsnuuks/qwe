a7ed6860a6706a844c88ad45616f974d1add0d64
xsnuuks <xsnuuks@gmail.com>
2026-08-18 04:19:28 +0200

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -1,3 +1,4 @@
+
 from aiogram.dispatcher.event.bases import SkipHandler
 from aiogram import Router, F, Bot
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
