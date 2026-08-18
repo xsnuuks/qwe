39513fce7dffed71a9ba853d3008f6ab5b843372
xsnuuks <xsnuuks@gmail.com>
2026-08-18 02:17:00 +0200

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -12,7 +12,7 @@ from database import (
 )
 from keyboards import (
     language_keyboard, main_menu_keyboard, catalog_keyboard,
-    product_keyboard, back_keyboard, cart_keyboard
+    product_keyboard, back_keyboard, cart_keyboard, city_keyboard
 )
 from locales import get_text
 
