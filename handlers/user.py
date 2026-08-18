23af527635bdbaddf12788e4c3bb76007da9d80f
xsnuuks <xsnuuks@gmail.com>
2026-08-18 04:38:53 +0200

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -291,7 +291,7 @@ async def cart_add(callback: CallbackQuery, lang: str):
     await callback.answer("✅ Добавлено в корзину", show_alert=False)
 
 
-@router.message(F.text.in_(["Корзина", "CART"]))
+@router.message(F.text.contains("орзин") | F.text == "CART")
 async def show_cart(message: Message, lang: str):
     cart = await get_cart(message.from_user.id)
     if not cart:
